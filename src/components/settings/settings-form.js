ed4bbb468747b3b56c0417e434448c2e518e7691
Michael Scholtz <michael.scholtz@outlook.com>
2016-11-06 12:24:35 +0100

    disable network settings in production demo mode

--- src/components/settings/settings-form.js
+++ src/components/settings/settings-form.js
@@ -40,37 +40,40 @@ class SettingsForm extends Component {
     return transportLayers;
   }
 
+  _areNetworkSettingsDisabled(isProduction, isDemoMode) {
+    return isProduction && isDemoMode;
+  }
+
   render() {
     const transportLayers = this._getTransportLayers(isProduction, isDemoMode);
+    const networkSettingsAreDisabled = this._areNetworkSettingsDisabled(isProduction, isDemoMode);
 
     return (
-      <div>
-        <Form onSubmit={this._handleSubmit.bind(this)}>
-          <Segment>
-            <Header size='medium'>General</Header>
-            <Form.Checkbox label='Check for updates on initial load' name='checkForUpdatesOnInitialLoad' defaultChecked={this.state.settings.checkForUpdatesOnInitialLoad} />
-          </Segment>
-          <Segment>
-            <Header size='medium'>Connection</Header>
-            <Form.Group widths='equal'>
-              <Form.Input label='Kodi IP Address' name='ipAddress' defaultValue={this.state.settings.ipAddress} />
-              <Form.Select label='Transport Layer' name='transportLayer' options={transportLayers} defaultValue={this.state.settings.transportLayer} />
-            </Form.Group>
-            <Form.Group widths='equal'>
-              <Form.Input label='WebSocket Port' name='webSocketPort' type='number' defaultValue={this.state.settings.webSocketPort} />
-              <Form.Input label='AJAX Port' name='ajaxPort' type='number' defaultValue={this.state.settings.ajaxPort} />
-            </Form.Group>
-          </Segment>
-          <Segment>
-            <Header size='medium'>Display</Header>
-            <Form.Group widths='equal'>
-              {/* <Form.Input label='Items per page' name='itemsPerPage' type='number' defaultValue={this.state.settings.itemsPerPage} /> */}
-              <Form.Input label='Items per row' name='itemsPerRow' type='number' defaultValue={this.state.settings.itemsPerRow} />
-            </Form.Group>
-          </Segment>
-          <Button primary type='submit'>Save</Button>
-        </Form>
-      </div>
+      <Form onSubmit={this._handleSubmit.bind(this)}>
+        <Segment>
+          <Header size='medium'>General</Header>
+          <Form.Checkbox label='Check for updates on initial load' name='checkForUpdatesOnInitialLoad' defaultChecked={this.state.settings.checkForUpdatesOnInitialLoad} />
+        </Segment>
+        <Segment>
+          <Header size='medium'>Connection</Header>
+          <Form.Group widths='equal'>
+            <Form.Input label='Kodi IP Address' name='ipAddress' defaultValue={this.state.settings.ipAddress} disabled={networkSettingsAreDisabled} />
+            <Form.Select label='Transport Layer' name='transportLayer' options={transportLayers} defaultValue={this.state.settings.transportLayer} />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Input label='WebSocket Port' name='webSocketPort' type='number' defaultValue={this.state.settings.webSocketPort} disabled={networkSettingsAreDisabled} />
+            <Form.Input label='AJAX Port' name='ajaxPort' type='number' defaultValue={this.state.settings.ajaxPort} disabled={networkSettingsAreDisabled} />
+          </Form.Group>
+        </Segment>
+        <Segment>
+          <Header size='medium'>Display</Header>
+          <Form.Group widths='equal'>
+            {/* <Form.Input label='Items per page' name='itemsPerPage' type='number' defaultValue={this.state.settings.itemsPerPage} /> */}
+            <Form.Input label='Items per row' name='itemsPerRow' type='number' defaultValue={this.state.settings.itemsPerRow} />
+          </Form.Group>
+        </Segment>
+        <Button primary type='submit'>Save</Button>
+      </Form>
     );
   }
 }
